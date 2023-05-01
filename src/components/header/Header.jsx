f717706c6f6cc7abcba8e722026b7008640507d5
ThanhVuNe <tranthanhvu7939@gmail.com>
2023-05-02 02:46:42 +0700

    change spring

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -87,7 +87,8 @@ const Header = () => {
         <div className="logo">
           <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcJV8o6WqlDiYhUlLyRcwKxRvj1S36C6a3_g&usqp=CAU'} alt="logo" />
           {/* <Link to={`/`}>POL</Link> */}
-          <a href="">POL</a>
+          
+          <Link to={`/`}>POL</Link>
         </div>
         
         <ul className="header__nav">
