97e3ad551e212aae38f333000f08711b716c4b35
ThanhVuNe <tranthanhvu7939@gmail.com>
2023-05-02 02:02:53 +0700

    change spring

--- src/api/springApi.js
+++ src/api/springApi.js
@@ -1,41 +1,41 @@
 
 const springApi = {
     getAccountByEmail: (email) => {
-        return fetch('http://localhost:8080/api/login/getByEmail/' + email)
+        return fetch('http://movie-group8.up.railway.app/api/login/getByEmail/' + email)
             .then(response => response.json())
     },
     getAll: () => {
-        return fetch('http://localhost:8080/api/movie/getAll')
+        return fetch('http://movie-group8.up.railway.app/api/movie/getAll')
             .then(response => response.json())
     },
     getMovieByGenre: (genre) => {
-       return fetch('http://localhost:8080/api/movie/getMovieByGenres/' + genre)
+       return fetch('http://movie-group8.up.railway.app/api/movie/getMovieByGenres/' + genre)
             .then(response => response.json())
     },
     getMovieByCategory: (category) => {
-        return fetch('http://localhost:8080/api/movie/getMovieByCategory/' + category)
+        return fetch('http://movie-group8.up.railway.app/api/movie/getMovieByCategory/' + category)
             .then(response => response.json())
     },
     getDetail: (id) => {
-        return fetch('http://localhost:8080/api/movie/getMovie/' + id)
+        return fetch('http://movie-group8.up.railway.app/api/movie/getMovie/' + id)
             .then(response => response.json())
     },
     getMovieByKeyword: (keyword) => {
-        return fetch('http://localhost:8080/api/movie/getMovieByKeyword/' + keyword)
+        return fetch('http://movie-group8.up.railway.app/api/movie/getMovieByKeyword/' + keyword)
             .then(response => response.json())
     },
     getAllCategory: () => {
-        return fetch('http://localhost:8080/api/category/getAll')
+        return fetch('http://movie-group8.up.railway.app/api/category/getAll')
             .then(response => response.json())
     },
     getCastByMovie: (id) =>{
-        return fetch('http://localhost:8080/api/cast/byMovie/' + id).then(response => response.json())
+        return fetch('http://movie-group8.up.railway.app/api/cast/byMovie/' + id).then(response => response.json())
     },
     getCategoryByMovie: (id) =>{
-        return fetch('http://localhost:8080/api/category/byMovie/' + id).then(response => response.json())
+        return fetch('http://movie-group8.up.railway.app/api/category/byMovie/' + id).then(response => response.json())
     },
     getCommentsByMovie: (id) => {
-        return fetch('http://localhost:8080/api/comment/byMovieDtoOrderByTime/' + id).then(response => response.json())
+        return fetch('http://movie-group8.up.railway.app/api/comment/byMovieDtoOrderByTime/' + id).then(response => response.json())
     }
     
 }
