a26e9cb29de7c8c0ccd7f7850f14f693a642c2ae
ThanhVuNe <tranthanhvu7939@gmail.com>
2023-05-02 14:03:22 +0700

    change style

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
+import "./watch.scss"
 const Watch = (props) => {
     console.log(props.location.state);
 
     return (
         <div className="mb-3 container" style={{margin: "130px 50px"}}>
             <h2 style={{fontSize: "30px", marginBottom: "30px"}}>Movie: {props.location.state.videoName}</h2>
-              <iframe
+              <iframe className="iframe-video"
                     // src={`https://www.youtube.com/embed/${videoLink}`}
                     src={props.location.state.link}
                     height={800}
