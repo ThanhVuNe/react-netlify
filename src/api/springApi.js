194d7ebf7eac170ba337c97119cbb8bc5e0b253c
ThanhVuNe <tranthanhvu7939@gmail.com>
2023-06-16 16:16:33 +0700

    first commit

--- src/api/springApi.js
+++ src/api/springApi.js
@@ -1,41 +1,41 @@
 
 const springApi = {
     getAccountByEmail: (email) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/login/getByEmail/' + email)
+        return fetch('https://api-movie-group-8.up.railway.app/api/login/getByEmail/' + email)
             .then(response => response.json())
     },
     getAll: () => {
-        return fetch('https://api-movie-group8.up.railway.app/api/movie/getAll')
+        return fetch('https://api-movie-group-8.up.railway.app/api/movie/getAll')
             .then(response => response.json())
     },
     getMovieByGenre: (genre) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/movie/getMovieByGenres/' + genre)
+        return fetch('https://api-movie-group-8.up.railway.app/api/movie/getMovieByGenres/' + genre)
             .then(response => response.json())
     },
     getMovieByCategory: (category) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/movie/getMovieByCategory/' + category)
+        return fetch('https://api-movie-group-8.up.railway.app/api/movie/getMovieByCategory/' + category)
             .then(response => response.json())
     },
     getDetail: (id) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/movie/getMovie/' + id)
+        return fetch('https://api-movie-group-8.up.railway.app/api/movie/getMovie/' + id)
             .then(response => response.json())
     },
     getMovieByKeyword: (keyword) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/movie/getMovieByKeyword/' + keyword)
+        return fetch('https://api-movie-group-8.up.railway.app/api/movie/getMovieByKeyword/' + keyword)
             .then(response => response.json())
     },
     getAllCategory: () => {
-        return fetch('https://api-movie-group8.up.railway.app/api/category/getAll')
+        return fetch('https://api-movie-group-8.up.railway.app/api/category/getAll')
             .then(response => response.json())
     },
     getCastByMovie: (id) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/cast/byMovie/' + id).then(response => response.json())
+        return fetch('https://api-movie-group-8.up.railway.app/api/cast/byMovie/' + id).then(response => response.json())
     },
     getCategoryByMovie: (id) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/category/byMovie/' + id).then(response => response.json())
+        return fetch('https://api-movie-group-8.up.railway.app/api/category/byMovie/' + id).then(response => response.json())
     },
     getCommentsByMovie: (id) => {
-        return fetch('https://api-movie-group8.up.railway.app/api/comment/byMovieDtoOrderByTime/' + id).then(response => response.json())
+        return fetch('https://api-movie-group-8.up.railway.app/api/comment/byMovieDtoOrderByTime/' + id).then(response => response.json())
     }
 
 }
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,7 @@
 
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import React, { useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./detail.scss";
 import CastList from "./CastList";
 import springApi from "../api/springApi";
@@ -11,9 +11,8 @@ const Detail = () => {
   // console.log(category, id);
   const [item, setItem] = useState(null);
   const [categorys, setCategorys] = useState(null);
-  const [videoName, setVideoName] = useState('');
   useEffect(() => {
-  
+
     const getDetail = async () => {
       console.log('getDetail');
       // const response = await springApi.getDetail(id);
@@ -21,14 +20,13 @@ const Detail = () => {
       response.then((res) => {
         console.log(res);
         setItem(res);
-        setVideoName(res.title + " trailer");
       });
     };
     getDetail();
   }, [category, id]);
 
   useEffect(() => {
-  
+
     const getCategory = async () => {
       // const response = await springApi.getDetail(id);
       const response = springApi.getCategoryByMovie(id)
@@ -89,23 +87,20 @@ const Detail = () => {
 
           <div className="container">
             <div className="section mb-3">
-              {/* <VideoList id={item.id} />
-                   */}
-              {videoName ? (
-                <div className="video">
-                  <div className="video__title">
-                    <h2>{videoName}</h2>
-                  </div>
-                  <iframe
-                    // src={`https://www.youtube.com/embed/${videoLink}`}
-                    src={item.trailer}
-                    height={800}
-                    width="100%"
-                    title="video"
-                  ></iframe>
+
+              <div className="video">
+                <div className="video__title">
+                  <h2>{item.title + " trailer"}</h2>
                 </div>
-              ) : (<h2 style={{ fontSize: "30px", textAlign: "center" }}>No Trailer</h2>)
-              }
+                <iframe
+                  // src={`https://www.youtube.com/embed/${videoLink}`}
+                  src={item.trailer}
+                  height={800}
+                  width="100%"
+                  title="video"
+                ></iframe>
+              </div>
+
 
             </div>
 
